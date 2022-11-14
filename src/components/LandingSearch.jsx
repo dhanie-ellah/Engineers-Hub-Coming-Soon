4e4430f30524e720e8148f91c0fba673b5458009
dhanie-ellah <dannygodwin2003@gmail.com>
2022-11-14 11:51:43 +0100

    style: landingSearch flex direction

--- src/components/LandingSearch.jsx
+++ src/components/LandingSearch.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import landingSearch from '../assets/landing-search.png'
+import landingSearch from '../assets/landing-search.png'
 import Envelope from '../assets/envelope.png'
-import lightSquare from '../assets/3.png'
-import darkSquare from '../assets/2.png'
-import landingImage from '../assets/1.png'
+// import lightSquare from '../assets/3.png'
+// import darkSquare from '../assets/2.png'
+// import landingImage from '../assets/1.png'
 
 const LandingSearch = () => {  
   return (
-    <div className='p-10 w-4/5 m-auto my-12 flex justify-between lg:flex-col lg:w-full lg:p-5 lg:gap-2'>
+    <div className='p-10 w-4/5 m-auto my-12 flex justify-between lg:flex-col-reverse lg:w-full lg:p-5 lg:gap-2'>
       <div className='w-6/12 flex flex-col gap-3 px-12 py-5 lg:w-full lg:px-0 '>
         <h1 className='text-blue text-2xl font-semibold'>The Engineers Community</h1>
         <h1 className='text-orange text-5xl font-bold'>Coming Soon</h1>
@@ -21,11 +21,11 @@ const LandingSearch = () => {
         </form>
       </div>
 
-      <div className='w-4/12 h-fit relative lg:w-11/12'>
-        {/* <img className='w-full flex items-center justify-center' src={landingSearch} alt="" /> */}
-        <img src={lightSquare} alt="" className='absolute top-10 left-10 animate-[wiggle_1s_ease-in-out_infinite]'/>
-        <img src={darkSquare} alt="" className='absolute top-5 left-5 animate-[wiggle_1s_ease-in-out_infinite]'/>
-        <img src={landingImage} alt="" className='absolute top-0 left-0'/>
+      <div className='w-4/12 h-fit relative lg:w-full'>
+        <img className='w-full flex items-center justify-center' src={landingSearch} alt="" />
+        {/* <img src={lightSquare} alt="" className='absolute top-10 left-10'/>
+        <img src={darkSquare} alt="" className='absolute top-5 left-5'/>
+        <img src={landingImage} alt="" className='absolute top-0 left-0'/> */}
       </div>
     </div>
   )
