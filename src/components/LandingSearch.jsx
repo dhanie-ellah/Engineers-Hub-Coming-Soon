10547944a7e5ea5e15f10b26188fdae987ea1752
dhanie-ellah <dannygodwin2003@gmail.com>
2022-12-05 03:23:29 +0100

    Style: Responsive styling update

--- src/components/LandingSearch.jsx
+++ src/components/LandingSearch.jsx
@@ -11,7 +11,7 @@ const LandingSearch = () => {
   return (
     <div className="p-10 py-5 w-4/5 m-auto my-5 flex justify-between items-center lg:flex-col-reverse lg:w-full lg:p-5 lg:gap-1 lg:my-3">
       <div className="w-[65%] flex flex-col gap-1.5 px-12 py-5 lg:w-full lg:px-0 lg:gap-1.5">
-        <h1 className="text-blue w-10/12 lg:w-full text-2xl lg:text-3xl md:text-xl font-semibold lg:text-center">
+        <h1 className="text-blue w-10/12 lg:w-full text-2xl lg:text-4xl md:text-xl font-semibold lg:text-center">
           Online Social Platform For Engineers.
         </h1>
         <h1 className="text-orange text-4xl lg:text-5xl md:text-4xl font-bold lg:text-center">
@@ -25,16 +25,16 @@ const LandingSearch = () => {
             <img
               src={Envelope}
               alt=""
-              className="absolute top-3 left-2.5 w-3.5 lg:top-4 md:top-3.5"
+              className="absolute top-3 left-2.5 w-3.5 lg:w-5 lg:top-[18px] lg:left-3.5 md:top-3.5 md:w-3.5"
             />
             <input
-              className="w-[75%] rounded-full py-1.5 lg:py-3 md:py-2 px-7 placeholder:text-dark-grey placeholder:text-xs border text-dark-grey outline-none border-dark-grey text-sm lg:w-full"
+              className="w-[75%] rounded-full py-1.5 lg:py-4 lg:px-10 md:py-2 px-7 placeholder:text-dark-grey placeholder:text-xs lg:placeholder:text-base border text-dark-grey outline-none border-dark-grey text-sm lg:w-full md:placeholder:text-xs md:px-8"
               type="text"
               placeholder="Enter Email Address"
             />
           </div>
           <input
-            className="bg-orange w-fit text-xs rounded-full px-5 py-2 lg:py-3 cursor-pointer md:py-2.5 hover:bg-orange-80 lg:m-auto"
+            className="bg-orange w-fit text-xs rounded-full px-5 py-2 lg:py-3 cursor-pointer md:py-2.5 hover:bg-orange-80 lg:m-auto lg:text-base md:text-xs"
             type="submit"
             value="Subscribe"
           />
@@ -88,7 +88,7 @@ const LandingSearch = () => {
           </div>
         </div>
 
-        <h1 className="text-blue font-semibold text-sm">
+        <h1 className="text-blue font-semibold text-sm lg:text-lg md:text-sm">
           All engineers in one platform.
         </h1>
       </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({setShowModal}) => {
       onClick={() => setShowModal(false)}
     >
       <div
-        className="w-4/12 bg-blue rounded-lg p-10 lg:p-5 flex flex-col gap-5 md:w-11/12 lg:py-6 lg:w-6/12 lg:rounded-3xl"
+        className="w-4/12 bg-blue rounded-lg p-10 md:p-5 flex flex-col gap-5 md:w-11/12 lg:py-6 lg:rounded-3xl lg:w-[75%]"
         onClick={(e) => e.stopPropagation()}
       >
         <img
@@ -18,10 +18,10 @@ const Modal = ({setShowModal}) => {
           className="bg-orange rounded-full p-1 w-7 ml-auto flex justify-end"
           onClick={() => setShowModal(false)}
         />
-        <h1 className="text-4xl font-bold text-orange text-center">
+        <h1 className="text-4xl font-bold text-orange text-center lg:text-6xl md:text-4xl">
           COMING SOON
         </h1>
-        <p className="font-semi-bold text-dark-grey text-sm text-center">
+        <p className="font-semi-bold text-dark-grey text-sm text-center lg:text-lg md:text-sm">
           Be the first to hear about latest updates, opportunities and freebies
           from Engineers Hub when you subscribe to our News letter
         </p>
@@ -30,16 +30,16 @@ const Modal = ({setShowModal}) => {
             <img
               src={Envelope}
               alt=""
-              className="absolute top-2 left-3.5 w-4"
+              className="absolute top-3 left-3.5 w-5"
             />
             <input
-              className="w-full rounded-full py-1.5 px-10 pr-2 placeholder:text-dark-grey placeholder:text-xs text-dark-grey outline-none text-xs"
+              className="w-full rounded-full py-1.5 px-10 pr-2 placeholder:text-dark-grey placeholder:text-xs text-dark-grey outline-none text-xs lg:py-3 lg:placeholder:text-base md:placeholder:text-xs"
               type="text"
               placeholder="Enter Email Address"
             />
           </div>
           <input
-            className="bg-orange text-xs rounded-full text-center py-2 cursor-pointer rounded-l-none w-1/4 lg:w-4/12 hover:bg-orange-80"
+            className="bg-orange text-xs rounded-full text-center py-2 cursor-pointer rounded-l-none w-1/4 lg:w-4/12 hover:bg-orange-80 lg:py-3 lg:text-base md:text-xs"
             type="submit"
             value="Subscribe"
           />
